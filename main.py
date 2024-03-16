21445b471f9b0973ef8abf1984e04b04e4520dc0
Kevin Liu <MagnificentKevin@hotmail.com>
2024-03-16 10:34:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import cv2 as cv
 vid = cv.VideoCapture("testvideo.mp4")
 while vid.isOpen():
     ret,frame = vid.read()
+    #error handeling:
+    if not ret:
+        print("i hate pythin sythax and yes its pythin")
+        break
+    #i miss simicolons...
+    
