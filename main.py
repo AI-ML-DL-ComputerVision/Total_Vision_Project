6160c52a30d948ddc0608a75754c1cb8016abb6a
Kevin Liu <MagnificentKevin@hotmail.com>
2024-03-16 10:43:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2 as cv
 
 vid = cv.VideoCapture("testvideo.mp4")
-while vid.isOpen():
+while vid.isOpened():
     ret,frame = vid.read()
     #error handeling:
     if not ret:
         print("i hate pythin sythax and yes its pythin")
         break
     #i miss semicolons...
-    
+    cv.imshow("pythin",frame)
