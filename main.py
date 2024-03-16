df7eeb3d019359a309addb343154dfcd5ff1d757
Kevin Liu <MagnificentKevin@hotmail.com>
2024-03-16 11:11:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2 as cv
 
 vid = cv.VideoCapture("testvideo2.mp4")
+
+
 while vid.isOpened():
     ret,frame = vid.read()
     #error handeling:
@@ -9,8 +11,13 @@ while vid.isOpened():
         print("i hate pythin sythax and yes its pythin")
         break
     #i miss semicolons...
+
+    detector = cv.SimpleBlobDetector()
     grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow("pythin",grayscale)
+    keypoints = detector.detect(grayscale)
+    final = cv2.drawKeypoints(grayscale, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)    
+
+    cv.imshow("pythin",final)
 
 vid.release()
 cv.destroyAllWindows()
